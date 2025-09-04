e01fab4f105f6ea585d17f0e32078589cd1fa1c9
efendy <efendy.setiawan@gmail.com>
2025-09-04 17:37:37 +0800

    configuration added

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,13 +1,34 @@
-const config: { upload: any } = {
+export default ({ env }) => ({
   upload: {
     config: {
       breakpoints: {
         small: 256,
         medium: 512,
-        large: 1024
+        large: 1024,
       },
+      ...(env('NODE_ENV') === 'production'
+        ? {
+            baseUrl: env('CDN_URL'),
+            rootPath: env('CDN_ROOT_PATH', ''),
+            provider: 'aws-s3',
+            providerOptions: {
+              endpoint: env('STORAGE_ENDPOINT'),
+              region: env('STORAGE_REGION'),
+              credentials: {
+                accessKeyId: env('STORAGE_KEY'),
+                secretAccessKey: env('STORAGE_SECRET'),
+              },
+              params: {
+                Bucket: env('STORAGE_BUCKET'),
+              },
+            },
+          }
+        : {
+            provider: 'local',
+            providerOptions: {
+              sizeLimit: 10 * 1024 * 1024, // 10 MB
+            },
+          }),
     },
   },
-};
-
-export default config;
+});
